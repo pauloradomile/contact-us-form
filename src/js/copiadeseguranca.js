33af9043a73032666787862e8c0c6157e24a81bf
pauloradomile <61995799+pauloradomile@users.noreply.github.com>
2023-11-25 00:21:22 +0000

    terminado passo 2 de 3 com sucesso

--- src/js/copiadeseguranca.js
+++ src/js/copiadeseguranca.js
@@ -6,30 +6,19 @@ const $stepThree = $('.step.three');
 
 const $containerBtnFormOne = $('#containerBtnFormOne');
 const $btnFormOne = $('#btnFormOne');
-const $containerBtnFormTwo = $('#containerBtnFormTwo');
-const $btnFormTwo = $('#btnFormTwo');
 const $inputNome = $('#nome');
 const $inputSobrenome = $('#sobrenome');
 const $inputDataNascimento = $('#dataNascimento');
 const $inputEmail = $('#email');
 const $inputMinibio = $('#minibio');
-const $inputEndereco = $('#endereco');
-const $inputComplento = $('#complemento');
-const $inputCidade = $('#cidade');
-const $inputCep = $('#cep');
 
 let nomeValido = false;
 let sobrenomeValido = false;
 let dataNascimentoValido = false;
 let emailValido = false;
-let enderecoValido = false;
-let cidadeValido = false;
-let cepValido = false;
 
 const minLegthText = 2;
-const minLegthTextArea = 10;
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-const cepRegex = /^([\d]{2})([\d]{3})([\d]{3})|^[\d]{2}.[\d]{3}-[\d]{3}/;
 
 function validarInput(element, minLength, maxLength, regex) {
     const closest = $(element).closest('.input-data');
@@ -48,31 +37,10 @@ function ValidaFormularioUm() {
     if (nomeValido && sobrenomeValido && emailValido && dataNascimentoValido) {
         $containerBtnFormOne.removeClass('disabled');
         $btnFormOne.removeClass('disabled');
-        $btnFormOne.off('click').on('click', IniciarFormularioDois);
     } else {
         $containerBtnFormOne.addClass('disabled');
         $btnFormOne.addClass('disabled');
-        $btnFormOne.off('click');
-
     }
-}
-
-function ValidaFormularioDois(){
-    if (enderecoValido && cidadeValido && cepValido){
-        $containerBtnFormTwo.removeClass('disabled');
-        $btnFormTwo.removeClass('disabled');       
-
-    }else{
-        $containerBtnFormTwo.addClass('disabled');
-        $btnFormTwo.addClass('disabled');
-    }
-}
-
-function IniciarFormularioDois() {
-    $stepText.text('Passo 2 de 3 - Dados de Correspondência');
-    $stepDescription.text('Precisamos dos dados de correnpondência para que possamos entrar em contacto caso seja necessário.');
-    $stepOne.hide();
-    $stepTwo.show();
 }
 
 function init() {
@@ -108,29 +76,6 @@ function init() {
 
     $inputMinibio.keyup(function () {
         ValidaFormularioUm();
-    });
-
-    $inputEndereco.keyup(function (){
-        enderecoValido = validarInput(this, minLegthTextArea);
-        ValidaFormularioDois();
-    })
-
-    $inputComplento.keyup(function (){
-        ValidaFormularioDois();
-    })
-
-    $inputCidade.keyup(function (){
-        cidadeValido = validarInput(this, minLegthText);
-        ValidaFormularioDois();
-    })
-
-    $inputCep.keyup(function () {
-        this.value = this.value.replace(/\D/g, '');
-        cepValido = validarInput(this, null, null, cepRegex);
-        if(cepValido){
-            this.value = this.value.replace(cepRegex, "$1.$2-$3");
-         }
-         ValidaFormularioDois();       
     });
 
     $inputDataNascimento.on('focus', function () {
